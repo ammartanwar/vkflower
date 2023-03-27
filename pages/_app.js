6d22fb043fdf5a57dbbaba9f18c96cffb2a08251
Ammar <Ammu@Simrans-MacBook-Air.local>
2023-03-27 23:32:23 +0530

    analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import React from 'react'
+import Script from "next/script";
 
 function _app({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+  <>
+  <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-E5FB2V31SY`}
+      />
+
+      <Script
+      id="my-script"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-E5FB2V31SY');`,
+        }}
+      />
+  <Component {...pageProps} />
+  </>
+);}
 
 export default _app
